a5bfb831068d934023ffcb9ab5dfd75693c90b67
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-04-14 13:37:42 +0430

    57-client-store-cookie lesson

--- components/AuthButton.jsx
+++ components/AuthButton.jsx
@@ -4,10 +4,13 @@ import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const AuthButton = () => {
+    const router = useRouter();
     // const successLogin = credentialResponse => {
     //     const userInfo = jwtDecode(credentialResponse.credential); // it can be run in server for login or signup
     //     console.log(userInfo);
@@ -44,8 +47,15 @@ const AuthButton = () => {
     //     //     onError={catchLogin}
     //     // />
     // );
+    const token = 'asdfjejkdndjwkencsenfcksdnkjsend...isejmwekfmwefmedji';
+    const cookieStore = new Cookies(null, { path: '/' });
+    cookieStore.set('loginToken', token);
+    const signInHandler = () => {
+        router.push('/userpanel');
+        signIn();
+    };
     return (
-        <button className='bg-red-300 rounded-md h-12 w-full' onClick={() => signIn()}>Sign-in</button>
+        <button className='bg-red-300 rounded-md h-12 w-full' onClick={() => signInHandler()}>Sign-in</button>
     )
 };
 
